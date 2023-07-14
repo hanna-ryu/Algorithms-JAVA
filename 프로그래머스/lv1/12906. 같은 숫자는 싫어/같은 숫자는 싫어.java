38f179f4abd3b0b2204d59ec2612d89ea6658720
Ryanna <121004473+hanna-ryu@users.noreply.github.com>
2023-07-14 13:40:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.01 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
