4e0f74ec438a49d494336414b4913021b2e2e2e3
Ryanna <121004473+hanna-ryu@users.noreply.github.com>
2023-07-20 11:16:42 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.24 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] answer = my_string.split("");
+        String temp = answer[num1];
+        
+        answer[num1] = answer[num2];
+        answer[num2] = temp;
+        
+        String answer2 = String.join("", answer);
+        
+        return answer2;
+    }
+}
