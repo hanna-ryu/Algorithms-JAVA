a354cdf1e4efae5494a27a3e04ba9951d9762508
Ryanna <121004473+hanna-ryu@users.noreply.github.com>
2023-10-31 20:27:58 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -5,13 +5,7 @@ class Solution {
         for(int i = 0 ; i < nums.length - 2; i ++){
             for(int j = i+1 ; j< nums.length - 1 ; j++){
                 for (int k = j+1; k < nums.length ; k++){
-                    if(isPrimeNumber(nums[i] + nums[j] + nums[k]) == true){
-//                         System.out.println("i : "  + nums[i] );
-//                         System.out.println("j : "  + nums[j] );
-//                         System.out.println("k : "  + nums[k] );
-                        
-//                         System.out.println(nums[i] + nums[j] + nums[k]);
-//                         System.out.println(isPrimeNumber(nums[i] + nums[j] + nums[k]) == true);
+                    if(isPrimeNumber(nums[i] + nums[j] + nums[k])){
                         answer += 1 ;
                     }
                 }
